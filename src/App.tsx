53cc2e1bc6ae0b264eb4ad8ef804ed0f6b0e2382
jiango <jrj444@foxmail.com>
2020-06-28 18:22:53 +0800

    封装底部导航栏为Nav组件

--- src/App.tsx
+++ src/App.tsx
@@ -3,10 +3,10 @@ import {
   HashRouter as Router,
   Switch,
   Route,
-  Link,
   Redirect
 } from 'react-router-dom';
 import styled from 'styled-components';
+import Nav from './components/Nav';
 
 const Wrapper = styled.div`
   height: 100vh;
@@ -16,18 +16,6 @@ const Wrapper = styled.div`
 
 const Main = styled.main`
   flex-grow: 1;
-`;
-
-const Nav = styled.nav`
-  border: 1px solid blue;
-  > ul {
-    display: flex;
-    > li{
-      flex-grow: 1;
-      padding: 16px;
-      text-align: center;
-    }
-  }
 `;
 
 function App() {
@@ -51,19 +39,7 @@ function App() {
             </Route>
           </Switch>
         </Main>
-        <Nav>
-          <ul>
-            <li>
-              <Link to="/tags">标签</Link>
-            </li>
-            <li>
-              <Link to="/money">记账</Link>
-            </li>
-            <li>
-              <Link to="/statistics">统计</Link>
-            </li>
-          </ul>
-        </Nav>
+        <Nav/>
       </Wrapper>
     </Router>
   );
---
+++ src/components/Nav.tsx
@@ -0,0 +1,35 @@
+import styled from 'styled-components';
+import {Link} from 'react-router-dom';
+import React from 'react';
+
+const NavWrapper = styled.nav`
+  box-shadow: 0 0 3px rgba(0,0,0,0.25);
+  > ul {
+    display: flex;
+    > li{
+      flex-grow: 1;
+      padding: 16px;
+      text-align: center;
+    }
+  }
+`;
+
+function Nav() {
+  return (
+    <NavWrapper>
+      <ul>
+        <li>
+          <Link to="/tags">标签</Link>
+        </li>
+        <li>
+          <Link to="/money">记账</Link>
+        </li>
+        <li>
+          <Link to="/statistics">统计</Link>
+        </li>
+      </ul>
+    </NavWrapper>
+  );
+}
+
+export default Nav;
