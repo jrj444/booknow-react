eec5e5693bc97d299d5cab6dd64fa937105a4dd6
jiango <jrj444@foxmail.com>
2020-06-28 17:48:11 +0800

    写好了底部标签栏的基础样式

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,88 @@
 import React from 'react';
+import {
+  HashRouter as Router,
+  Switch,
+  Route,
+  Link,
+  Redirect
+} from 'react-router-dom';
+import styled from 'styled-components';
+
+const Wrapper = styled.div`
+  height: 100vh;
+  display: flex;
+  flex-direction: column;
+`;
+
+const Main = styled.main`
+  flex-grow: 1;
+`;
+
+const Nav = styled.nav`
+  border: 1px solid blue;
+  > ul {
+    display: flex;
+    > li{
+      flex-grow: 1;
+      padding: 16px;
+      text-align: center;
+    }
+  }
+`;
 
 function App() {
   return (
-    <div></div>
+    <Router>
+      <Wrapper>
+        <Main>
+          <Switch>
+            <Route path="/tags">
+              <Tags/>
+            </Route>
+            <Route path="/money">
+              <Money/>
+            </Route>
+            <Route path="/statistics">
+              <Statistics/>
+            </Route>
+            <Redirect exact from="/" to="/money"/>
+            <Route path="*">
+              <NoMatch/>
+            </Route>
+          </Switch>
+        </Main>
+        <Nav>
+          <ul>
+            <li>
+              <Link to="/tags">标签</Link>
+            </li>
+            <li>
+              <Link to="/money">记账</Link>
+            </li>
+            <li>
+              <Link to="/statistics">统计</Link>
+            </li>
+          </ul>
+        </Nav>
+      </Wrapper>
+    </Router>
   );
+}
+
+function Tags() {
+  return <h2>标签页</h2>;
+}
+
+function Money() {
+  return <h2>记账页</h2>;
+}
+
+function Statistics() {
+  return <h2>统计页</h2>;
+}
+
+function NoMatch() {
+  return <h2>地址无效，请重新输入有效地址</h2>;
 }
 
 export default App;
