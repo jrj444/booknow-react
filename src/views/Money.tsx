5baeb46300357ffa4912b9d0787e40ec4bc4e507
jiango <jrj444@foxmail.com>
2020-06-29 20:08:32 +0800

    完成备注输入功能

--- src/views/Money.tsx
+++ src/views/Money.tsx
@@ -2,9 +2,9 @@ import Layout from '../components/Layout';
 import React from 'react';
 import styled from 'styled-components';
 import {CategorySection} from './Money/CategorySection';
-import {NotesSection} from './Money/NotesSection';
 import {NumberPadSection} from './Money/NumberPadSection';
 import {TagsSection} from './Money/TagsSection';
+import {NoteSection} from './Money/NoteSection';
 
 const MyLayout = styled(Layout)`
   display: flex;
@@ -15,12 +15,7 @@ function Money() {
   return (
     <MyLayout>
       <TagsSection/>
-      <NotesSection>
-        <label>
-          <span>备注</span>
-          <input type="text" placeholder="在这里添加备注"/>
-        </label>
-      </NotesSection>
+      <NoteSection/>
       <CategorySection>
         <ul>
           <li className="selected">支出</li>
