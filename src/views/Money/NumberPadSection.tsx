b175e653180c6fd7edf077ddad3bf9c68dfc06af
jiango <jrj444@foxmail.com>
2020-06-29 22:09:14 +0800

    完成数字键盘按钮基本逻辑

--- src/views/Money/NumberPadSection.tsx
+++ src/views/Money/NumberPadSection.tsx
@@ -1,6 +1,7 @@
 import styled from 'styled-components';
+import React, {useState} from 'react';
 
-const NumberPadSection = styled.section`
+const Wrapper = styled.section`
   display: flex;
   flex-direction: column;
   > .output {
@@ -28,5 +29,78 @@ const NumberPadSection = styled.section`
     }
   }
 `;
+
+const NumberPadSection = () => {
+  const [output, _setOutput] = useState('0');
+  const setOutput = (stringNum: string) => {
+    if (stringNum.length > 16) {
+      stringNum = stringNum.slice(0, 16);
+    } else if (stringNum.length === 0) {
+      stringNum = '0';
+    }
+    _setOutput(stringNum);
+  };
+  const onNumberClick = (e: React.MouseEvent) => {
+    const text = (e.target as HTMLButtonElement).textContent;
+    if (text === null) {return;}
+    switch (text) {
+      case '0':
+      case '1':
+      case '2':
+      case '3':
+      case '4':
+      case '5':
+      case '6':
+      case '7':
+      case '8':
+      case '9':
+        if (output === '0') {
+          setOutput(text);
+        } else {
+          setOutput(output + text);
+        }
+        break;
+      case '.':
+        if (output.indexOf('.') >= 0) {return;}
+        setOutput(output + '.');
+        break;
+      case 'OK':
+
+        break;
+      case '删除':
+        if (output.length === 1) {
+          setOutput('0');
+        } else {
+          setOutput(output.slice(0, -1));
+        }
+        break;
+      case '清空':
+        setOutput('0');
+        break;
+    }
+
+  };
+  return (
+    <Wrapper>
+      <div className="output">{output}</div>
+      <div className="pad clearfix" onClick={onNumberClick}>
+        <button>7</button>
+        <button>8</button>
+        <button>9</button>
+        <button>删除</button>
+        <button>4</button>
+        <button>5</button>
+        <button>6</button>
+        <button>清空</button>
+        <button>1</button>
+        <button>2</button>
+        <button>3</button>
+        <button className="ok">OK</button>
+        <button className="zero">0</button>
+        <button>.</button>
+      </div>
+    </Wrapper>
+  );
+};
 
 export {NumberPadSection};
