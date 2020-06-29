4b14e379b69d775045055c948727dde6f3689293
jiango <jrj444@foxmail.com>
2020-06-29 11:07:43 +0800

    完成底部导航栏 icon

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -2,14 +2,27 @@ import styled from 'styled-components';
 import {Link} from 'react-router-dom';
 import React from 'react';
 
+require('assets/icons/money.svg');
+require('assets/icons/tags.svg');
+require('assets/icons/statistics.svg');
+
 const NavWrapper = styled.nav`
   box-shadow: 0 0 3px rgba(0,0,0,0.25);
   > ul {
     display: flex;
     > li{
       flex-grow: 1;
-      padding: 16px;
       text-align: center;
+      display: flex;
+      flex-direction: column;
+      align-items: center;
+      justify-content: center;
+      padding: 6px 0;
+      > .nav-icon{
+        width: 24px;
+        height: 24px;
+        margin-bottom: 2px;
+      }
     }
   }
 `;
@@ -19,12 +32,21 @@ function Nav() {
     <NavWrapper>
       <ul>
         <li>
+          <svg className="nav-icon">
+            <use xlinkHref="#tags"/>
+          </svg>
           <Link to="/tags">标签</Link>
         </li>
         <li>
+          <svg className="nav-icon">
+            <use xlinkHref="#money"/>
+          </svg>
           <Link to="/money">记账</Link>
         </li>
         <li>
+          <svg className="nav-icon">
+            <use xlinkHref="#statistics"/>
+          </svg>
           <Link to="/statistics">统计</Link>
         </li>
       </ul>
