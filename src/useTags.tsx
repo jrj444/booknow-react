0e90452e8e3ce7e08b13df0c15e16b15ef3ffcfb
jiango <jrj444@foxmail.com>
2020-06-30 14:54:40 +0800

    设置默认 tags ，防止多次初始化

--- src/useTags.tsx
+++ src/useTags.tsx
@@ -1,13 +1,15 @@
 import {useState} from 'react';
 import {createID} from './lib/createID';
 
+const defaultTags = [
+  {id: createID(), name: '购物'},
+  {id: createID(), name: '饮食'},
+  {id: createID(), name: '住宿'},
+  {id: createID(), name: '交通'},
+];
+
 const useTags = () => {
-  const [tags, setTags] = useState<{ id: number, name: string }[]>([
-    {id: createID(), name: '购物'},
-    {id: createID(), name: '饮食'},
-    {id: createID(), name: '住宿'},
-    {id: createID(), name: '交通'},
-  ]);
+  const [tags, setTags] = useState<{ id: number, name: string }[]>(defaultTags);
   return {tags, setTags};
 };
 
