4156f475f12a5411d473f6b16c3ece3270226614
jiango <jrj444@foxmail.com>
2020-06-30 11:21:19 +0800

    完成 标签页 样式

--- src/views/Tags.tsx
+++ src/views/Tags.tsx
@@ -1,12 +1,65 @@
 import Layout from '../components/Layout';
 import React from 'react';
 import {useTags} from '../useTags';
+import styled from 'styled-components';
+import Icon from '../components/Icon';
+import {Link} from 'react-router-dom';
+
+const TagList = styled.ol`
+  font-size: 16px; 
+  background: #ffffff;
+  > li {
+    border-bottom: 1px solid #d5d5d5;
+    line-height: 20px;
+    margin-left: 16px;
+    > a {
+      padding: 12px 16px 12px 0;
+      display: flex;
+      justify-content: space-between;
+      align-items: center;
+    }
+    > span {
+      white-space: nowrap;
+      overflow: hidden;
+      text-overflow: ellipsis;
+    }
+  }
+`;
+
+const Button = styled.button`
+  font-size: 18px;
+  border: none;
+  padding: 8px 12px;
+  background: #027aff;
+  color: #ffffff;
+  border-radius: 8px;
+`;
+
+const Center = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  margin-top: 45px;
+`;
 
 function Tags() {
   const {tags, setTags} = useTags();
+  console.log(setTags);
   return (
     <Layout>
-      <h2>标签页面</h2>
+      <TagList>
+        {tags.map(tag =>
+          <li key={tag}>
+            <Link to={'/tags/' + tag}>
+              <span>{tag}</span>
+              <Icon name="right"/>
+            </Link>
+          </li>
+        )}
+      </TagList>
+      <Center>
+        <Button>新增标签</Button>
+      </Center>
     </Layout>
   );
 }
