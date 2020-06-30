1ed18df3fdb1dbadd196b65526433a3f196f2a3c
jiango <jrj444@foxmail.com>
2020-06-30 21:49:02 +0800

    优化更新、删除标签函数

--- src/useTags.tsx
+++ src/useTags.tsx
@@ -22,16 +22,10 @@ const useTags = () => {
     return result;
   };
   const updateTag = (id: number, name: string) => {
-    const index = findTagIndex(id);
-    const tagsClone = JSON.parse(JSON.stringify(tags));
-    tagsClone.splice(index, 1, {id: id, name: name});
-    setTags(tagsClone);
+    setTags(tags.map(tag => tag.id === id ? {id, name} : tag));
   };
   const deleteTag = (id: number) => {
-    const index = findTagIndex(id);
-    const tagsClone = JSON.parse(JSON.stringify(tags));
-    tagsClone.splice(index, 1);
-    setTags(tagsClone);
+    setTags(tags.filter(tag => tag.id !== id));
   };
   return {tags, setTags, findTag, updateTag, findTagIndex, deleteTag};
 };
