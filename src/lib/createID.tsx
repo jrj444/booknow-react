68d26ccffc79b5978a9b06575c1d023fde9f604c
jiango <jrj444@foxmail.com>
2020-07-01 16:57:49 +0800

    将标签ID的最大值存入localStorage，防止每次刷新都从1开始

--- src/lib/createID.tsx
+++ src/lib/createID.tsx
@@ -1,6 +1,7 @@
-let id = 0;
+let id = parseInt(window.localStorage.getItem('max_id') || '0');
 const createID = () => {
   id += 1;
+  window.localStorage.setItem('max_id', JSON.stringify(id));
   return id;
 };
 
