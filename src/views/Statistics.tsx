e2cd06f1130c4a835b810e68c41f0e1bd61df917
jiango <jrj444@foxmail.com>
2020-07-03 21:03:10 +0800

    完成统计列表排序显示

--- src/views/Statistics.tsx
+++ src/views/Statistics.tsx
@@ -42,8 +42,8 @@ function Statistics() {
   });
   // @ts-ignore
   const array = Object.entries(hash).sort((a, b) => {
-    if (a[0] < b[0]) {return -1;}
-    if (a[0] > b[0]) {return 1;}
+    if (a[0] < b[0]) {return 1;}
+    if (a[0] > b[0]) {return -1;}
     return 0;
   });
 
