b35d75b60dc1870885c6e93e2eb85deab78020c4
jrj444 <jrj444@foxmail.com>
2020-12-15 18:00:20 +0800

    Updates

---
+++ precache-manifest.596b42181f520bd056db1c6731cc7105.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5d1818af37c6e2c373b8cc295d163ce8",
+    "url": "/booknow-react/index.html"
+  },
+  {
+    "revision": "40d5d4eb1143259fa7c5",
+    "url": "/booknow-react/static/css/main.079a52e4.chunk.css"
+  },
+  {
+    "revision": "5186f829a0c1d9497003",
+    "url": "/booknow-react/static/js/2.4a2b4a1c.chunk.js"
+  },
+  {
+    "revision": "99406820fc9e02f862dbd66e126ca114",
+    "url": "/booknow-react/static/js/2.4a2b4a1c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "40d5d4eb1143259fa7c5",
+    "url": "/booknow-react/static/js/main.b9b18c57.chunk.js"
+  },
+  {
+    "revision": "4700428a69ff81f83bbd",
+    "url": "/booknow-react/static/js/runtime-main.800c4450.js"
+  }
+]);
